fe66cf214600ac05083e2e748e1e8e0f3af43ab0
1mustyz <onemusty.z@gmail.com>
2021-06-21 20:42:59 +0100

    classes

--- routes/AdminRoute.js
+++ routes/AdminRoute.js
@@ -6,6 +6,7 @@ const { isLoggedIn } = require('../middlewares/auth');
 const studentController = require('../controller/studentController')
 const curriculumController = require('../controller/curriculumController');
 const subjectController = require('../controller/subjectController');
+const classController = require('../controller/classController')
 
 router.post('/register-staff', staffController.registerStaff)
 router.post('/register-student', studentController.registerStudent)
@@ -16,6 +17,9 @@ router.post('/logout', logoutController.logout)
 router.post('/add-payment', paymentController.addPaymentTypes)
 router.post('/add-curriculum', curriculumController.create)
 router.post('/create-subject', subjectController.create)
+router.post('/create-class', classController.create)
+router.post('/verify-payment', paymentController.verifyPayment);
+
 // admin set his profile
 router.put('/set-profile-pic', staffController.setProfilePic);
 
@@ -23,11 +27,11 @@ router.put('/set-profile-pic', staffController.setProfilePic);
 router.put('/set-role', staffController.setRole);
 
 router.put('/update-payment', paymentController.updatePaymentTypes)
-router.post('/verify-payment', paymentController.verifyPayment);
 
 router.put('/update-single-student-profile', studentController.updateSingleStudent);
 router.put('/update-single-curriculum', curriculumController.updateSingleCurriculum);
 router.put('/update-single-subject', subjectController.update)
+router.put('/update-class', classController.update)
 
 router.get('/get-all-curriculum', curriculumController.getAllCurriculum)
 router.get('/get-single-curriculum', curriculumController.getSingleCurriculum)
@@ -39,10 +43,12 @@ router.get('/get-all-staff', staffController.findAllStaff);
 router.get('/get-all-student', studentController.findAllStudent);
 router.get('/get-single-student', studentController.findOneStudent);
 router.get('/get-all-subject', subjectController.getAllSubject)
+router.get('/get-all-classes', classController.getAllClasses)
 
 router.delete('/remove-student', studentController.removeStudent)
 router.delete('/delete-single-curriculum', curriculumController.deleteSingleCurriculum)
 router.delete('/delete-all-curriculum', curriculumController.deleteAllCurriculum)
 router.delete('/delete-single-subject', subjectController.delete)
+router.delete('/delete-class', classController.delete)
 
 module.exports = router;
