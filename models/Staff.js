78d0de310011bf0863ec841f28b7861bb36bc825
1mustyz <onemusty.z@gmail.com>
2021-06-24 22:37:23 +0100

    paid statistics

--- models/Staff.js
+++ models/Staff.js
@@ -26,7 +26,7 @@ const StaffSchema = new Schema({
     nextKinAddress: { type: String},
     image: { type: String, default: '1.jpg' },
     role: { type: Array, default: 'none'},
-    teach: [{class: {type:String}, subject: {type:Array}}]
+    teach: [{class: {type:String}, subject: {type:Array}, category: {type:String, default: 'none'}}]
 }, { timestamps: true });   
 
 //plugin passport-local-mongoose to enable password hashing and salting and other things
