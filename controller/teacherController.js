700425f07e25d49893b075ab73df5b9b5ea5a6ee
1mustyz <onemusty.z@gmail.com>
2021-06-27 09:47:15 +0100

    updated role

--- controller/teacherController.js
+++ controller/teacherController.js
@@ -21,6 +21,8 @@ exports.fetchStudentsInClass = async (req, res) => {
 
 exports.liveSaveResult = async (req, res) => {
     const field = req.body.key
+    const value = req.body.value
+
 
     // i added this to update total
     const result = await Score.findById(req.body.id,{total: 1})
@@ -28,15 +30,15 @@ exports.liveSaveResult = async (req, res) => {
 
     const score = await Score.findByIdAndUpdate(req.body.id, {
         // i added the update total field
-        [field]: req.body.value, total: result.total + field
+        [field]: req.body.value, total: result.total + value
     }, {new: true, useFindAndModify: false})
 
-    await TermResult.findByIdAndUpdate(req.body.studentId, {total: result.total + field})
+    await TermResult.findOneAndUpdate({
+        username: req.body.username}, {total: result.total + value})
 
-    const termResult = await TermResult.findById(
-        req.body.studentId,{average: 1, noOfCourse: 1, total: 1})
-    
-        await TermResult.findByIdAndUpdate(req.body.studentId, {average: termResult.total / termResult.noOfCourse})    
+    const termResult = await TermResult.find({
+        username: req.body.username},{average: 1, noOfCourse: 1, total: 1})
+        // await TermResult.findByIdAndUpdate(req.body.studentId, {average: termResult.total / termResult.noOfCourse})    
     res.json({ success: true, score })
 
 
