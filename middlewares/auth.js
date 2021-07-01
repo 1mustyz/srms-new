1aca8c204fbf4bf9cca7f2f251d2767d4c04a87f
sulaiman <sulaimanaminu02@gmail.com>
2021-07-01 08:24:46 +0100

    lets go

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,7 +1,7 @@
 //check if user is logged in && the user is not trying to access unauthorised data
 exports.isLoggedIn = function (role) {
     return (req, res, next) => {
-      if (req.isAuthenticated() && req.user.role === role) {
+      if (req.isAuthenticated() && req.user.role.includes(role)) {
         return res.json({ success: true, message: 'valid user' })
       }
       res.json({ success: false, message: 'unauthorized or authenticated user'})
