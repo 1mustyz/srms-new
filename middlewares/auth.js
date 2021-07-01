595f6e38b2b2577aa307b604ee07d4465146f711
1mustyz <onemusty.z@gmail.com>
2021-07-01 09:56:03 +0100

    general result

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,9 +1,9 @@
 //check if user is logged in && the user is not trying to access unauthorised data
 exports.isLoggedIn = function (role) {
     return (req, res, next) => {
-      if (req.isAuthenticated() && req.user.role.includes(role)) {
+      if (req.isAuthenticated()) {
         return res.json({ success: true, message: 'valid user' })
       }
-      res.json({ success: false, message: 'unauthorized or authenticated user', req.user})
+      res.json({ success: false, message: 'unauthorized or authenticated user', user: req.user})
     }
   }
