9324e49b10d059f8c7e5428ac9eefeb19d86a64e
1mustyz <onemusty.z@gmail.com>
2021-07-10 06:44:37 +0100

    general result

--- controller/curriculumController.js
+++ controller/curriculumController.js
@@ -2,9 +2,12 @@ const Curriculum = require('../models/Curriculum');
 
 
 exports.create = async (req,res,next) => {
-    const {section,name} = req.body
+    const {section,name,category} = req.body
 
-    await Curriculum.findOneAndDelete({name: name})
+    
+    if(section !== 'SSS') await Curriculum.findOneAndDelete({name: name})
+    else await Curriculum.findOneAndDelete({name: name,category: category})
+    
 
     section !== 'SSS'
      ? req.body.category = "none"
