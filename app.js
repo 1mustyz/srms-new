78501c4936cf1ade29597b583ab114980822d74a
1mustyz <onemusty.z@gmail.com>
2022-09-08 12:45:59 +0100

    setting up first sprint

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.use(cors())
 app.use(expressSession({
   secret: '[credentials.secret]',
   store: MongoStore.create({
-    mongoUrl: 'mongodb+srv://1mustyz:z08135696959@project1.ynhhl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+    mongoUrl: 'mongodb+srv://1mustyz:z08135696959@exchange.xzo88tb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
     ttl: 14 * 24 * 60 * 60,
     autoRemove: 'native',
   }),
@@ -41,7 +41,7 @@ const teacherRouter = require('./routes/teacherRoute')
 // mongodb://localhost:27017/newsrms
 
 // //connect to db
-mongoose.connect('mongodb+srv://1mustyz:z08135696959@project1.ynhhl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
+mongoose.connect('mongodb+srv://1mustyz:z08135696959@exchange.xzo88tb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true
